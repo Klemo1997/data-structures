d7fb0e4ee9904fcb8892d186d3ba5ceac51bb6e6
klemo1997 <matuskoklemo@gmail.com>
2021-10-10 11:04:33 +0200

    refactor: Linked list tests cleanup and format

--- linked_list/test/test_linked_list.c
+++ linked_list/test/test_linked_list.c
@@ -5,19 +5,17 @@
 #include "linked_list.h"
 
 CHEAT_TEST(linked_list_creation_works,
-        cheat_assert_int(2, linked_list_create_node(2)->data);
+    cheat_assert_int(2, linked_list_create_node(2)->data);
 )
 
 CHEAT_DECLARE(
-    int testArr[1] = {1};
-    int testTwoArray[1] = {2};
-    int test2Array[2] = {1, 2};
-    int testEmptyArray[0] = {};
+    int singleElementArray[1] = {1};
+    int twoElementsArray[2] = {1, 2};
 )
 
 CHEAT_DECLARE(
-    struct Node* getPrependedListFromIntArray(const int array[], const int size) {
-        struct Node* linked_list = NULL;
+    struct Node *getPrependedListFromIntArray(const int array[], const int size) {
+        struct Node *linked_list = NULL;
 
         for (int i = 0; i < size; i++) {
             linked_list_prepend(&linked_list, array[i]);
@@ -28,79 +26,79 @@ CHEAT_DECLARE(
 )
 
 CHEAT_TEST(linked_list_prepend_works,
-    cheat_assert_int(getPrependedListFromIntArray(testArr, 1)->data, 1);
+    cheat_assert_int(getPrependedListFromIntArray(singleElementArray, 1)->data, 1);
 )
 
 CHEAT_TEST(linked_list_multi_prepend_works,
-   cheat_assert_int(getPrependedListFromIntArray(test2Array, 2)->data, 2);
-   cheat_assert_int(getPrependedListFromIntArray(test2Array, 2)->next->data, 1);
+    cheat_assert_int(getPrependedListFromIntArray(twoElementsArray, 2)->data, 2);
+    cheat_assert_int(getPrependedListFromIntArray(twoElementsArray, 2)->next->data, 1);
 )
 
 CHEAT_DECLARE(
-        struct Node* getAppendedListFromIntArray(const int array[], const int size) {
-            struct Node* linked_list = NULL;
+    struct Node *getAppendedListFromIntArray(const int array[], const int size) {
+        struct Node *linked_list = NULL;
 
-            for (int i = 0; i < size; i++) {
-                linked_list_append(&linked_list, array[i]);
-            }
-
-            return linked_list;
+        for (int i = 0; i < size; i++) {
+            linked_list_append(&linked_list, array[i]);
         }
+
+        return linked_list;
+    }
 )
 
 CHEAT_TEST(linked_list_append_works,
-   cheat_assert_int(getAppendedListFromIntArray(testArr, 1)->data, 1);
+    cheat_assert_int(getAppendedListFromIntArray(singleElementArray, 1)->data, 1);
 )
 
 CHEAT_TEST(linked_list_multi_append_works,
-   cheat_assert_int(getAppendedListFromIntArray(test2Array, 2)->data, 1);
-   cheat_assert_int(getAppendedListFromIntArray(test2Array, 2)->next->data, 2);
+    cheat_assert_int(getAppendedListFromIntArray(twoElementsArray, 2)->data, 1);
+    cheat_assert_int(getAppendedListFromIntArray(twoElementsArray, 2)->next->data, 2);
 )
 
 CHEAT_DECLARE(
-        struct Node* deleteArrayFromLinkedList(struct Node* linked_list, const int array[], const int size) {
-            for (int i = 0; i < size; i++) {
-                linked_list_delete(&linked_list, array[i]);
-            }
-
-            return linked_list;
+    struct Node *deleteArrayFromLinkedList(struct Node *linked_list, const int array[], const int size) {
+        for (int i = 0; i < size; i++) {
+            linked_list_delete(&linked_list, array[i]);
         }
+
+        return linked_list;
+    }
 )
 
 CHEAT_TEST(linked_list_delete_on_empty_linked_list_works,
-    struct Node* empty_linked_list = NULL;
+    struct Node *empty_linked_list = NULL;
     cheat_assert_ptrdiff(
-    (ptrdiff_t) deleteArrayFromLinkedList(empty_linked_list, testArr, 1),
-    (ptrdiff_t) NULL
+           (ptrdiff_t) deleteArrayFromLinkedList(empty_linked_list, singleElementArray, 1),
+           (ptrdiff_t) NULL
     );
 )
 
 CHEAT_TEST(linked_list_delete_single_node_works,
-    struct Node* empty_linked_list = NULL;
+    struct Node *empty_linked_list = NULL;
     cheat_assert_ptrdiff(
        (ptrdiff_t) deleteArrayFromLinkedList(
-               getAppendedListFromIntArray(testArr, 1),
-               testArr,
+               getAppendedListFromIntArray(singleElementArray, 1),
+               singleElementArray,
                1
-           ),
+       ),
        (ptrdiff_t) NULL
     );
 )
 
 CHEAT_TEST(linked_list_delete_head_works,
-    struct Node* linked_list = getAppendedListFromIntArray(test2Array, 2);
+    struct Node *linked_list = getAppendedListFromIntArray(twoElementsArray, 2);
     linked_list_delete(&linked_list, 1);
     cheat_assert_int(
-       linked_list->data,
-       2
+           linked_list->data,
+           2
     );
 )
 
 CHEAT_TEST(linked_list_delete_tail_works,
-    struct Node* linked_list = getAppendedListFromIntArray(test2Array, 2);
+    struct Node *linked_list = getAppendedListFromIntArray(twoElementsArray, 2);
     linked_list_delete(&linked_list, 2);
     cheat_assert_int(
-       linked_list->data,
-       1
+           linked_list->data,
+           1
     );
 )
