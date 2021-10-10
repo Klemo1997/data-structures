077ca957aaac1379b03a19f00b8c36b281200b04
klemo1997 <matuskoklemo@gmail.com>
2021-10-10 14:49:59 +0200

    refactor: Add example linked list usage to main

--- linked_list/src/main.c
+++ linked_list/src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #include "linked_list.h"
 
 
 int main() {
+    struct Node* head = NULL;
+    linked_list_append(&head, 2);
+    linked_list_prepend(&head, 1);
+    linked_list_print(head);
+    linked_list_destroy(&head);
 
     return 0;
 }
