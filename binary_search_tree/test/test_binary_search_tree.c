a4f5f17457f7fdcbc218572cb97a23948434eed0
klemo1997 <matuskoklemo@gmail.com>
2021-10-14 22:11:01 +0200

    test: Add test for duplicate insertion cases

--- binary_search_tree/test/test_binary_search_tree.c
+++ binary_search_tree/test/test_binary_search_tree.c
@@ -21,3 +21,13 @@ CHEAT_TEST(binary_search_tree_insertion_multiple_inserts,
     cheat_assert_int(root->left->data, 1);
     cheat_assert_int(root->right->data, 3);
 )
+
+CHEAT_TEST(binary_search_tree_insertion_does_not_add_duplicates,
+    struct TreeNode *root = NULL;
+    binary_search_tree_insert(&root, 2);
+    binary_search_tree_insert(&root, 2);
+
+    cheat_assert_int(root->data, 2);
+    cheat_assert(root->left == NULL);
+    cheat_assert(root->right == NULL);
+)
