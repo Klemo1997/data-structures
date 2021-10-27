4509e7d6452339984aff788e32aa40bd7556471e
klemo1997 <matuskoklemo@gmail.com>
2021-10-27 22:19:35 +0200

    refactor: Extract stack sequence popping tests into common function

--- stack/test/test_stack.c
+++ stack/test/test_stack.c
@@ -4,6 +4,29 @@
 #include "cheats.h"
 #include "stack.h"
 
+CHEAT_DECLARE(
+    void assert_stack_from_array(int8_t* stack_sequence, const int size) {
+        struct Stack** stack = Stack_create();
+
+        for (int i = 0; i < size; i ++) {
+            Stack_push(stack, stack_sequence[i]);
+        }
+
+        // Iterate in reverse because of stack LIFO nature
+        for (int i = size - 1; i <= 0; i++) {
+            int data = Stack_pop(stack);
+
+            cheat_assert_int(data, stack_sequence[i]);
+
+            if (i > 0) {
+                cheat_assert_int((*stack)->data, stack_sequence[i - 1]);
+            } else {
+                cheat_assert((*stack) == NULL);
+            }
+        }
+    }
+)
+
 CHEAT_TEST(it_pushes_one_item,
     struct Stack** stack = Stack_create();
 
@@ -37,23 +60,17 @@ CHEAT_TEST(it_pushes_three_items_in_sequence_correctly,
 CHEAT_TEST(it_pops_single_item_from_stack_and_deletes_ref,
    struct Stack** stack = Stack_create();
 
-   Stack_push(stack, 2);
-   int data = Stack_pop(stack);
+   int8_t stack_sequence[1] = {2};
 
-   cheat_assert_int(data, 2);
-   cheat_assert((*stack) == NULL);
+   assert_stack_from_array(stack_sequence, 1);
 )
 
 CHEAT_TEST(it_pops_top_and_sets_second_item_as_new_top,
     struct Stack** stack = Stack_create();
 
-    Stack_push(stack, 2);
-    Stack_push(stack, 3);
+    int8_t stack_sequence[2] = {2, 3};
 
-    int data = Stack_pop(stack);
-
-    cheat_assert_int(data, 3);
-    cheat_assert_int((*stack)->data, 2);
+    assert_stack_from_array(stack_sequence, 2);
 )
 
 CHEAT_TEST(it_pops_sequence_correctly,
@@ -61,20 +78,5 @@ CHEAT_TEST(it_pops_sequence_correctly,
 
     int8_t stack_sequence[3] = {1, 2, 3};
 
-    for (int i = 0; i < 3; i ++) {
-        Stack_push(stack, stack_sequence[i]);
-    }
-
-    // Iterate in reverse because of stack LIFO nature
-    for (int i = 2; i <= 0; i++) {
-        int data = Stack_pop(stack);
-
-        cheat_assert_int(data, stack_sequence[i]);
-
-        if (i > 0) {
-            cheat_assert_int((*stack)->data, stack_sequence[i - 1]);
-        } else {
-            cheat_assert((*stack) == NULL);
-        }
-    }
+    assert_stack_from_array(stack_sequence, 3);
 )
